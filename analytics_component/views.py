533d6c69994ff587717ad1ebe2da906506bad70f
--replace-all <thewildclub.oli@gmail.com>
2021-04-25 08:04:26 +0100

    Setup for analytics page

--- analytics_component/views.py
+++ analytics_component/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'analytics_component/index.html', context)
